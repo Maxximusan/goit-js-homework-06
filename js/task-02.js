b6c7d6ecdaf4c751be47afb7b7e7041e197b3183
Maxximusan <dragonstonecave@gmail.com>
2022-08-19 14:05:31 +0300

    2я задача - ок

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,33 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// const ulEl = document.querySelector('ul');
+// // console.log(ulEl)
+
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ing = ingredients[i];
+
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('item');
+//   itemEl.textContent = ing;
+//   elements.push(itemEl)
+// }
+// ulEl.append(...elements);
+// console.log(ulEl)
+
+const ulEl = document.querySelector('#ingredients');
+
+const elem = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+
+console.log(ulEl)
+console.log(elem)
+
+ulEl.append(...elem);
