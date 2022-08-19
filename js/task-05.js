1ae1146333ba6afc2c5b93700a0775b813bd4407
Maxximusan <dragonstonecave@gmail.com>
2022-08-19 20:38:58 +0300

    5я задача - ок
    console.log ?

---
+++ js/task-05.js
@@ -0,0 +1,22 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output')
+};
+
+
+
+refs.input.addEventListener('input', onInputChange);
+
+
+function onInputChange(event) {
+    refs.output.textContent = event.currentTarget.value;
+
+    if (refs.output.textContent === '') {
+        refs.output.textContent = 'Anonymous'
+    }
+
+}
+
+
+
+console.log();
