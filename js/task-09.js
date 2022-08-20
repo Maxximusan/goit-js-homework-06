ea672bf65e223f1221467828b71a37dadbe4986d
Maxximusan <dragonstonecave@gmail.com>
2022-08-20 16:44:17 +0300

    9я - ок

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+
+  btn: document.querySelector('.change-color'),
+
+  span: document.querySelector('.color')
+}
+
+
+refs.btn.addEventListener('click', onClick);
+
+function onClick(event) {
+  // const target = event.currentTarget
+  refs.span.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = refs.span.textContent
+  console.log(refs.body)
 }
